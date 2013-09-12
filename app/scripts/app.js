0ba210f04b248b9ba72346206b57dbba571efde2
Chris Root <chrisr1386@gmail.com>
2013-09-11 20:50:42 -0400

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('myApp', [])
+  .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+      $locationProvider.html5Mode(true);
+  }]);
